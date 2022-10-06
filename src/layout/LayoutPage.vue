01978f3f12ffb2341ed2ffabfb29bb32be7a06b3
pimvanroon <pimvanroon@gmail.com>
2022-10-06 21:15:22 +0200

    Initial commit

---
+++ src/layout/LayoutPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="layout-container">
+      <transition name="fade" mode="out-in">
+        <keep-alive include="dashboard">
+          <router-view />
+        </keep-alive>
+      </transition>
+  </div>
+</template>
+<script>
+export default {
+  name: 'LayoutPage',
+  data() {
+    return {
+      currentPage: 'dashboard'
+    }
+  },
+  watch: {
+    '$route': {
+      deep: true,
+      handler: function (page) {
+        this.currentPage = page.name;
+      }
+    }
+  }
+}
+</script>
